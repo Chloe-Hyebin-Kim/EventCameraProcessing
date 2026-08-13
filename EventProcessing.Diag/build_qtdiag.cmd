b0cdbb827b5e4d32605cca4fa8604739ee0effa0
Chloe Hyebin Kim <dev.5igner1101@gmail.com>
2026-08-13 22:12:53 +0900

    Clarify supported Qt kits for Visual Studio 2019

--- EventProcessing.Diag/build_qtdiag.cmd
+++ EventProcessing.Diag/build_qtdiag.cmd
@@ -12,13 +12,19 @@ if not defined QT_ROOT set "QT_ROOT=%QTDIR%"
 set "BUILD=%SOURCE%\build\vs-%CONFIG%"
 
 rem Qt VS Tools does not always export its selected Qt version to Makefile
-rem projects. If QTDIR is absent, try the conventional Qt 5 MSVC locations.
+rem projects. If QTDIR is absent, try supported Qt 5 MSVC locations. Qt 5.3
+rem predates VS 2019 and its official binary kits are not ABI-compatible here.
 if not defined QT_ROOT (
-    for /f "delims=" %%D in ('dir /b /s /ad "C:\Qt\5.*\msvc2019_64" 2^>nul') do set "QT_ROOT=%%D"
+    for /d %%V in ("C:\Qt\5.12*" "C:\Qt\5.13*" "C:\Qt\5.14*" "C:\Qt\5.15*") do (
+        if exist "%%~fV\msvc2019_64\lib\cmake\Qt5\Qt5Config.cmake" set "QT_ROOT=%%~fV\msvc2019_64"
+        if exist "%%~fV\msvc2017_64\lib\cmake\Qt5\Qt5Config.cmake" set "QT_ROOT=%%~fV\msvc2017_64"
+    )
 )
 
 if not defined QT_ROOT (
-    echo [qtDiag] ERROR: Qt 5 MSVC x64 was not found.
+    echo [qtDiag] ERROR: A supported Qt 5 MSVC x64 kit was not found.
+    echo [qtDiag] Qt 5.3.1 is too old for this VS 2019/CMake project.
+    echo [qtDiag] Install Qt 5.12 or newer, preferably Qt 5.15.2 msvc2019_64.
     echo [qtDiag] Set QTDIR to the kit directory, for example:
     echo [qtDiag]   set QTDIR=C:\Qt\5.15.2\msvc2019_64
     echo [qtDiag] Then restart Visual Studio and rebuild qtDiag.
@@ -31,9 +37,30 @@ if not exist "%QT_ROOT%\lib\cmake\Qt5\Qt5Config.cmake" (
     exit /b 3
 )
 
+for /f "delims=" %%V in ('"%QT_ROOT%\bin\qmake.exe" -query QT_VERSION 2^>nul') do set "QT_VERSION=%%V"
+if not defined QT_VERSION (
+    echo [qtDiag] ERROR: qmake could not determine the Qt version in "%QT_ROOT%".
+    exit /b 4
+)
+for /f "tokens=1,2 delims=." %%A in ("%QT_VERSION%") do (
+    set "QT_MAJOR=%%A"
+    set "QT_MINOR=%%B"
+)
+if not "%QT_MAJOR%"=="5" goto unsupported_qt
+if %QT_MINOR% LSS 12 goto unsupported_qt
+goto qt_version_ok
+
+:unsupported_qt
+echo [qtDiag] ERROR: Qt %QT_VERSION% is unsupported. Qt 5.12 or newer is required.
+echo [qtDiag] Qt 5.15.2 msvc2019_64 is recommended for Visual Studio 2019.
+exit /b 5
+
+:qt_version_ok
+
 echo [qtDiag] Source : %SOURCE%
 echo [qtDiag] Build  : %BUILD%
 echo [qtDiag] Qt     : %QT_ROOT%
+echo [qtDiag] Version: %QT_VERSION%
 
 if /i "%ACTION%"=="clean" (
     if exist "%BUILD%\CMakeCache.txt" cmake --build "%BUILD%" --config %CONFIG% --target clean
